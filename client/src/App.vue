49398b99bab3b7f34d674988015abc551656489f
Kipras Melnikovas <sarpixo@gmail.com>
2018-10-27 13:25:15 +0300

    Move Navbar to component & improve landing
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,45 @@
 <template>
   <div id="app">
 
-    <!-- TODO - Navbar componentas -->
+  <Navbar></Navbar>
 
     <main role="main">
-
-        <div class="container">
+        <div class="container-fluid">
           <router-view></router-view>
         </div>
 
     </main>
 
-  <!-- TODO - Footer componentas -->
-
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  components: { Navbar },
+};
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
 #app {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3e50;
+  color: #353b3c;
+  min-height: 100vh;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -89,7 +91,6 @@ footer {
 }
 
 footer p {
-  margin-bottom: .25rem;
+  margin-bottom: 0.25rem;
 }
-
 </style>
