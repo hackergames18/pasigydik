962fc0d22cf489f0271ae922253209d5bb34e020
Kipras Melnikovas <sarpixo@gmail.com>
2018-10-27 14:22:24 +0300

    Update Landing & slighly modify navbar
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- client/src/components/Landingas.vue
+++ client/src/components/Landingas.vue
@@ -7,16 +7,14 @@
           <div class="row">
             <div class="col-md-5">
               <div class="align-left">
-                <div class="landing-text">
-                  <h1>Medical attention from the comfort of your home.</h1>
-                </div>
+                <h1>Medical attention from the comfort of your home.</h1>
                 <button type="button" id="landing-signup" class="btn  btn-lg">
                   Sign up
                 </button>
               </div>
             </div>
             <div class="col-md-7">
-              <img src="" alt="">
+              <img src="../assets/landing.png" alt="" class="landing-img">
               TODO Image
             </div>
 
@@ -28,50 +26,110 @@
     </section>
 
     <section id="second">
-      <span class="text-center">
-        <h1>Choose from a variety of our specialists</h1>
-      </span>
+      <div class="container">
+        <h1 class="text-center">Choose from a variety of our specialists</h1>
+
+        <div class="row">
+          <span v-for="spec in specialists" :key="spec.id">
+            <button class=" specialist mx-2" type="button">
+              {{ spec }}
+            </button>
+          </span>
+        </div>
+
+        <!-- <div class="specialists1">
+          <span v-for="spec in specialists1" :key="spec.id">
+            <button class="specialist mx-2" type="button">
+              {{ spec }}
+            </button>
+          </span>
+        </div>
+        <div class="specialists2">
+          <span v-for="spec in specialists2" :key="spec.id">
+            <button class="specialist mx-2" type="button">
+              {{ spec }}
+            </button>
+          </span>
+        </div> -->
+
+        <h2 class="text-center ending">
+          and many more...
+        </h2>
+      </div>
     </section>
+
+    <div class="delete-me"></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      specialists1: ["Dentist", "Dietologist", "Cardiologist"],
+      specialists2: ["Allergist", "Psychologist", "Surgeon"],
+      specialists: [
+        "Dentist",
+        "Dietologist",
+        "Cardiologist",
+        "Allergist",
+        "Psychologist",
+        "Surgeon",
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
+.delete-me {
+  margin-bottom: 20em;
+}
+
 section#first {
   background-color: #f7f7f7;
   padding-bottom: 5em;
   min-height: 93vh;
+
+  .landing-img {
+    // TODO
+  }
+
+  .align-left {
+    margin-top: 40%;
+    text-align: left;
+  }
+
+  #landing-signup {
+    font-size: 1.5em;
+    padding: 0.2em 2em;
+    margin-top: 2em;
+    border-radius: 25px;
+    background-color: #2191fb;
+    color: #fff;
+  }
 }
 
 section#second {
-}
+  margin-top: 5em;
 
-.navbar-brand {
-  font-size: 2em;
-}
+  h1 {
+    font-weight: 500;
+  }
 
-.align-left {
-  margin-left: 1em;
-  text-align: left;
-}
+  button.specialist {
+    font-size: 1.5em;
+    padding: 0.4em 2em;
+    margin-top: 2em;
+    border-radius: 50px;
+    background-color: #2191fb;
+    color: #fff;
+    border: 0px solid #fff;
+  }
 
-.landing-text {
-  margin-top: 40%;
-}
-
-.landing-text h1 {
-  font-weight: 900;
-}
-
-#landing-signup {
-  font-size: 1.5em;
-  padding: 0.2em 2em;
-  margin-top: 2em;
-  border-radius: 25px;
-  background-color: #2191fb;
-  color: #fff;
+  .ending {
+    color: #bfbfbf;
+    margin-top: 2em;
+  }
 }
 </style>
