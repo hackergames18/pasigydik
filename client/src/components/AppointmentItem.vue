af734f495e115fe200b4285c83a39462d0873912
Kipras Melnikovas <sarpixo@gmail.com>
2018-10-28 11:13:02 +0200

    Create MyAppointments & AppointmentItem workflow
    Prop managment included. Will just need to style AppointmentItem.
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ client/src/components/AppointmentItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="test-data">
+    id: {{id}}
+    patientId: {{patientId}}
+    doctorId: {{doctorId}}
+    startTimeMins: {{startTimeMins}}
+    </div>
+  </div>
+</template>
+
+<script>
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
+export default {
+  // name: "AppointmentItem",
+  props: {
+    id: {
+      type: Number,
+      default: getRandomInt(1337420360),
+    },
+    patientId: {
+      type: Number,
+      default: 0,
+    },
+    doctorId: {
+      type: Number,
+      default: 1,
+    },
+    startTimeMins: {
+      // type: Date,
+      default: new Date(),
+    },
+    durationMins: {
+      // type: Date,
+      default: new Date(),
+    },
+    details: {
+      type: Object,
+      default: {},
+    },
+  },
+  data: function() {
+    return {
+      id: this.id,
+      patientId: this.patientId,
+      doctorId: this.doctorId,
+      startTimeMins: this.startTimeMins,
+      durationMins: this.durationMins,
+      details: this.details,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- client/src/components/MyAppointments.vue
+++ client/src/components/MyAppointments.vue
@@ -1,12 +1,87 @@
 <template>
   <div class="my-appointments">
-    <h1>Todo - MyAppointments</h1>
+    <h1>Appointment overview</h1>
+    <div class="scheduled-elapsed-selection">
+      <button id="scheduled" class="current" type="">
+        Scheduled
+      </button>
+      <button id="elapsed">
+        Elapsed
+      </button>
+    </div>
+
+    <div class="sorting">
+      <AppointmentItem
+      v-for="app in appointments"
+      v-bind:key="app.id"
+      v-bind:roomId="app.roomId"
+      v-bind:patientId="app.patientId"
+      v-bind:doctorId="app.doctorId"
+      v-bind:startTimeMins="app.startTimeMins"
+      v-bind:durationMins="app.durationMins"
+      v-bind:details="app.details"
+      ></AppointmentItem>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {};
+import AppointmentItem from "./AppointmentItem";
+
+export default {
+  data() {
+    return {
+      appointments: [
+        {
+          id: 1,
+          roomId: "randomRoomIdSameAsDoctors",
+          patientId: 123,
+          doctorId: 456,
+          starTimeMins: Math.floor(new Date().getTime() / 1000 / 60),
+          durationMins: 20,
+          details: {},
+        },
+        {
+          id: 2,
+          roomId: "randomRoomIdSameAsDoctorsV2",
+          patientId: 234,
+          doctorId: 567,
+          starTimeMins: Math.floor(new Date().getTime() / 1000 / 60) + 5,
+          durationMins: 20,
+          details: {},
+        },
+        {
+          id: 3,
+          roomId: "randomRoomIdSameAsDoctorsV3",
+          patientId: 159,
+          doctorId: 753,
+          starTimeMins: Math.floor(new Date().getTime() / 1000 / 60) + 10,
+          durationMins: 20,
+          details: {},
+        },
+      ],
+    };
+  },
+  components: { AppointmentItem },
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.my-appointments {
+  margin-top: 2em;
+
+  .scheduled-elapsed-selection {
+    button {
+      background-color: transparent;
+      border: none;
+      font-size: 1.4em;
+      font-weight: 100;
+      padding: 0 1em;
+    }
+    .current {
+      border-bottom: 5px solid #2191fb;
+    }
+  }
+}
 </style>
